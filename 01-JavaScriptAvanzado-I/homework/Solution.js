48c2f169d76411dcd64269d2f40ce0ef32b3a3f8
gdsergio0629 <godaddysergio0629@gmail.com>
2022-03-17 23:27:38 -0500

    First Excercise soluction

---
+++ 01-JavaScriptAvanzado-I/homework/Solution.js
@@ -0,0 +1,24 @@
+x = 1;
+var a = 5;
+var b = 10;
+var c = function(a, b, c) {
+  var x = 10;
+  console.log(x); // 10
+  console.log(a); // 8
+  var f = function(a, b, c) {
+    b = a;
+    console.log(b); // 8
+    b = c;
+    var x = 5;
+  }
+  f(a,b,c);
+  console.log(b); // 9
+}
+c(8,9,10);
+console.log(b); // 10
+console.log(x); // 1
+
+
+// ------------------------------------------------------------------
+// In the next link I've created a conceptual drawing describing step by step. 
+// https://excalidraw.com/#json=Z-9R4MsALxQSP9PBffk-r,rx9_PCACctusyMU3tpplgg
